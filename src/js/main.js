e2c9d07f1bed65ce750e6b6f3b928d0333e0f83f
Mikheil Meskhi <mishameskhi1@gmail.com>
2023-03-28 15:31:52 +0400

    add header and aside

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,17 @@ document.body.classList.remove("preload");
 
 //Header
 const hamburger = document.querySelector(".hamburger");
+const blankActive = document.querySelector(".blank-active");
 
 hamburger.addEventListener("click", function () {
   this.classList.toggle("active");
+  blankActive.classList.toggle("blank-deactivate");
 });
+
+const sidebar = document.querySelector("aside");
+
+function openSidebar() {
+  sidebar.classList.toggle("aside-active");
+}
+
+hamburger.addEventListener("click", openSidebar);
